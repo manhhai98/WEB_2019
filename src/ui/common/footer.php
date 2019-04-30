c4041eecb8cffd4cb17e62453ca62f8fe58530d1
ngthailam <ngthailam98@gmail.com>
2019-04-30 11:14:57 +0700

    Add php for menu.php

---
+++ src/ui/common/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div id="container">
+        <a href="index.php" class="logo"><img src="../../assets/img/Shulas-Restaurants-Hi-Res-Transparent-Background.png" alt="logo"></a>
+
+        <div class="icon" style="text-align: center;">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-google"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-youtube"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-pinterest"></a>
+        </div>
+    </div>
+</footer>
--- src/ui/front_end/post.php
+++ src/ui/front_end/post.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Post</title>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
 </head>
+
 <body>
 	<header>
 		<div id="container">
@@ -27,21 +29,25 @@
 	<div id="img">
 		<img src="img/images (1).jpg" alt="food" style="width: 100%; height: 500px; margin-top: 40px;">
 		<div class="center" style="font-size: 30px; font-family: none;">THE BEST IN TOWN</div>
-		
-		<div class="center" style="margin-top: 80px; font-size: 40px;"><h2>POST</h2></div>
+
+		<div class="center" style="margin-top: 80px; font-size: 40px;">
+			<h2>POST</h2>
+		</div>
 	</div>
 
 	<div id="main-content">
 		<div class="menu" style=" margin-bottom: 20px;border-bottom: solid;border-color: red;">
 			<ul>
-				<li><a href="#" class="item"><h1>ALL POSTS</h1></a></li>
+				<li><a href="#" class="item">
+						<h1>ALL POSTS</h1>
+					</a></li>
 				<li style="float: right;">
 					<div class="search-container">
-    					<form action="#">
-	      					<input type="text" placeholder="Search.." name="search">
-	      					<button type="submit"><i class="fa fa-search" style="color: white;"></i></button>
-    					</form>
-  					</div>
+						<form action="#">
+							<input type="text" placeholder="Search.." name="search">
+							<button type="submit"><i class="fa fa-search" style="color: white;"></i></button>
+						</form>
+					</div>
 				</li>
 			</ul>
 		</div>
@@ -49,7 +55,9 @@
 		<div class="post" style="background-color: #ffffff;">
 			<div class="item">
 				<a href="#"><img src="../../assets/img/katie-smith-104748-unsplash.jpg"></a>
-				<a href="#"><h1>Title</h1></a>
+				<a href="#">
+					<h1>Title</h1>
+				</a>
 				<i>20/09/2019</i>
 				<i>by admin</i>
 				<br>
@@ -58,7 +66,9 @@
 
 			<div class="item">
 				<a href="#"><img src="../../assets/img/katie-smith-104748-unsplash.jpg"></a>
-				<a href="#"><h1>Title</h1></a>
+				<a href="#">
+					<h1>Title</h1>
+				</a>
 				<i>20/09/2019</i>
 				<i>by admin</i>
 				<br>
@@ -67,7 +77,9 @@
 
 			<div class="item">
 				<a href="#"><img src="../../assets/img/katie-smith-104748-unsplash.jpg"></a>
-				<a href="#"><h1>Title</h1></a>
+				<a href="#">
+					<h1>Title</h1>
+				</a>
 				<i>20/09/2019</i>
 				<i>by admin</i>
 				<br>
@@ -76,20 +88,8 @@
 		</div>
 	</div>
 
-	<footer>
-		<div id="container">
-			<a href="index.php" class="logo"><img src="../../assets/img/Shulas-Restaurants-Hi-Res-Transparent-Background.png" alt="logo"></a>
-
-			<div class="icon" style="text-align: center;">
-				<a href="#" class="fa fa-facebook"></a>
-				<a href="#" class="fa fa-twitter"></a>
-				<a href="#" class="fa fa-google"></a>
-				<a href="#" class="fa fa-linkedin"></a>
-				<a href="#" class="fa fa-youtube"></a>
-				<a href="#" class="fa fa-instagram"></a>
-				<a href="#" class="fa fa-pinterest"></a>
-			</div>
-		</div>
-	</footer>
+	<?php include_once '../common/footer.php' ?>
+	
 </body>
+
 </html>
