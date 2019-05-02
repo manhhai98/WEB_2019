06984d9e772a546a0430d667a5725eac87cb6ab8
manhhai <39932592+manhhai98@users.noreply.github.com>
2019-05-02 08:12:55 +0700

    Doi ten menu

--- src/ui/front_end/menu.php
+++ src/ui/front_end/menu.php
@@ -7,7 +7,7 @@ include './menu_action.php';
 <html>
 
 <head>
-	<title>Menu</title>
+	<title>Thiss is a Menu</title>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
